89a4ab093570d72d68a7896d17012079f1996cd8
Miskota <miskota@stud.uni-obuda.hu>
2023-12-01 19:48:14 +0100

    Implemented RecordController

---
+++ D03CBX_HFT_2023241.Endpoint/Controllers/RecordController.cs
@@ -0,0 +1,50 @@
+﻿using D03CBX_HFT_2023241.Logic;
+using D03CBX_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace D03CBX_HFT_2023241.Endpoint.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase {
+        // GET: api/<RecordController>
+
+        IRecordLogic logic;
+
+        public RecordController(IRecordLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Record> ReadAll() {
+            return this.logic.ReadAll();
+        }
+
+        // GET api/<RecordController>/5
+        [HttpGet("{id}")]
+        public Record Read(int id) {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<RecordController>
+        [HttpPost]
+        public void Create([FromBody] Record value) {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<RecordController>/5
+        [HttpPut]
+        public void Update([FromBody] Record value) {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<RecordController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            this.logic.Delete(id);
+        }
+    }
+}
