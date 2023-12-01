521ae596a8a71f42220115b2db22764c05fd744d
Miskota <miskota@stud.uni-obuda.hu>
2023-12-01 19:55:38 +0100

    Implemented Writer Controller

---
+++ D03CBX_HFT_2023241.Endpoint/Controllers/WriterController.cs
@@ -0,0 +1,50 @@
+﻿using D03CBX_HFT_2023241.Logic;
+using D03CBX_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace D03CBX_HFT_2023241.Endpoint.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WriterController : ControllerBase {
+
+        IWriterLogic logic;
+        public WriterController(IWriterLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<WriterController>
+        [HttpGet]
+        public IEnumerable<Writer> ReadAll() {
+            return this.logic.ReadAll();
+        }
+
+        // GET api/<WriterController>/5
+        [HttpGet("{id}")]
+        public Writer Read(int id) {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<WriterController>
+        [HttpPost]
+        public void Create([FromBody] Writer value) {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<WriterController>/5
+        [HttpPut]
+        public void Update([FromBody] Writer value) {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<WriterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            this.logic.Delete(id);
+        }
+    }
+}
